b30c042dfb2113500b266552910f40d8026a7384
jamesleipold <james.leipold@hotmail.com>
2020-02-14 12:57:14 +0800

    Update EQ8-Driver.c
    Functionality moved from controller to driver.

--- EQ8-Driver.c
+++ EQ8-Driver.c
@@ -1,31 +1,87 @@
 /*--------------------------------------------------------------
-* Mount Controller
+* EQ8 - Driver
 * Author: Jim Leipold
 * Email: james.leipold@hotmail.com
 * Created on: 11/02/2020
 * Last modifiied on: 13/02/2020
-*
-* This program contains high level mount controlling functionality:
-*   - Parsing of text commands.
-    - Parsing of error codes
+* 
+* This library contains mid level mount controlling functionality:
+*   - Parsing of error codes.
 *   - Command retries and logical error handling.
-*   - 
+* This should be the highest-level library common to all controllers.
 *-------------------------------------------------------------*/
 
-#define VERSION_CONTROLLER 1.4
+#define VERSION_DRIVER 1.4
+#define MAX_INPUT 32
 #include "EQ8-Comms.c"
-void convert(char data[9])
+
+
+int kbhit()
 {
-    printf("%c%c%c%c%c%c\n", data[5], data[6], data[3], data[4], data[1], data[2]);
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
 }
 
+/* *
+ * Positional data sent from the mount is formatted:    0xefcdab
+ * This function reformats for readability to:          0xabcdef
+ * */
+void convert(char data_in[9])
+{
+    char data_out[7] = {data_in[5],
+                        data_in[6],
+                        data_in[3],
+                        data_in[4],
+                        data_in[1],
+                        data_in[2]};
+    printf("Converted:\t%s", data_out);
+}
+
+/* *
+ * Function to controller, call port setup.
+ * Returns port ID on success, -1 on failure.
+ * */
+int setup_Controller(void)
+{
+    int fd = setup_Port();
+
+    if (verbose)
+    {
+        printf("\nEQ8 Pro Mount Driver:\n");
+        printf("Version: %.2f\n\n", VERSION_DRIVER);
+    }
+    return fd;
+}
+
+/* *
+ * Function to shutdown port connection.
+ * Returns port ID on success, -1 on failure.
+ * */
+int shutdown_Controller(int port)
+{
+    return close(port); /* Close the serial port */
+}
+
+/* *
+ * Function to interpret response from mount
+ * Determines if response is error (and interprets),
+ * or success and presents data.
+ * Prints interpretation.
+ * Returns flag.
+ * */
 int parse_Response(struct response *response)
 {
     int flag = (*response).flag;
     char data[9];
     strcpy(data, (*response).data);
     printf("Response:\t%s\n", data);
-    if (flag == -1)
+    if (flag == -1 && verbose)
     {
         printf("DEBUG: Read Error");
     }
@@ -49,35 +105,111 @@ int parse_Response(struct response *response)
             if (data[1] == '5')
                 printf("Driver Sleeping\n");
         }
-        convert(data);
+        else if (flag && strlen(data) == 8)
+            convert(data);
     }
     return flag;
 }
 
-int send_Command(int port, char command[])
+/* *
+ * Function to send a command to the mount and recieve
+ * its response.
+ * Returns a pointer to the structure containing flag & data
+ * TODO: include retry functionality.
+ * Returns port ID on success, -1 on failure.
+ * */
+struct response *send_Command(int port, char command[])
 {
     TX(port, command);
     usleep(30000); // this gives the mount time to repsond
                    // else all responsed will be offset by 1 from their respective commands
                    // Should be able to replace this by changing serial to canonical mode
-    return parse_Response(RX(port));
+    return RX(port);
 }
 
-
-
-int setup_Controller(void)
+/* *
+ * Function to interpret keyboard commands.
+ * Allows for command strings to be defined for more complex
+ * or series of commands
+ * */
+void parse_Command(int port, char input[MAX_INPUT])
 {
-    int fd = setup_Port();
-    printf("\n--- EQ8 Pro Mount Controller ---\n");
-    if (verbose)
-        printf("Verbose Mode: on\n");
-    if (verbose)
-        printf("Version: %.2f\n", VERSION_CONTROLLER);
-    printf("----------------------------\n");
-    return fd;
+    char c = '\0';
+    if (strcasecmp(input, "position") == 0)
+    {
+        printf("Position Mode: \nPress 'c' to cancel\n");
+        while (c != '1' && c != '2')
+        {
+            printf("\rChannel 1 / 2? ");
+            c = getchar();
+        }
+        char channel[3];
+        channel[0] = 'j';
+        if (c == '1')
+            channel[1] = '1';
+        else if (c == '2')
+            channel[1] = '2';
+
+        system("/bin/stty raw");
+        do
+        {
+            printf("\rPosition:\t%s", (*send_Command(port, channel)).data);
+            fflush(stdout);
+        }  while (!(kbhit() && getchar() == 'c'));
+        system("/bin/stty cooked");
+    }
+
+    else if (strcasecmp(input, "manual") == 0)
+    {
+        printf("Manual Mode: Use 'l' and 'r' to move\nPress 'c' to cancel\n");
+        system("/bin/stty raw");
+
+        char c;
+        do
+        {
+            c = getchar();
+
+            if (c == 'l')
+            {
+                TX(port, "G230");
+                usleep(20000);
+                TX(port, "J2");
+                while (c == 'l')
+                {
+                    c = getchar();
+                }
+                TX(port, "K2");
+            }
+
+            else if (c == 'r')
+            {
+                TX(port, "G231");
+                usleep(30000);
+                TX(port, "J2");
+                while (c == 'r')
+                {
+                    c = getchar();
+                }
+
+                TX(port, "K2");
+                usleep(30000);
+            }
+        } while (c != 'c');
+        system("/bin/stty cooked");
+    }
+    else
+        parse_Response((send_Command(port, input)));
 }
 
-int shutdown_Controller(int port)
+int main(void)
 {
-    return close(port); /* Close the serial port */
+    system("clear");
+    char input[MAX_INPUT];
+    int fd = setup_Controller();
+    while (true)
+    {
+        printf("\nCommand:\t");
+        scanf("%s", input);
+        parse_Command(fd, input);
+    }
 }
