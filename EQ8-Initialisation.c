a3e7804a7470e13b9be96f196a4ea8cc9304fec4
jamesleipold <james.leipold@hotmail.com>
2020-04-30 13:47:20 +0800

    Added files
    to hold lock initialisation and slow feedback

---
+++ EQ8-Initialisation.c
@@ -0,0 +1,19 @@
+/*--------------------------------------------------------------
+* EQ8 - Inititilisation
+* Author: Jim Leipold
+* Email: james.leipold@hotmail.com
+* Created on: 12/03/2020
+* Last modifiied on: 17/03/2020
+* 
+* This library contains high level mount controlling functionality:
+*   
+*-------------------------------------------------------------*/
+#define VERSION_Slow-Feedback 2.0
+#include "EQ8-Driver.h"
+
+int scan(double resolution, double range)
+{
+
+
+}
+
