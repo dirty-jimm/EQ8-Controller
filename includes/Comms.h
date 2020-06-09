e6265209e40d87bf3a4cf4402eb49ce16c0d491a
jamesleipold <james.leipold@hotmail.com>
2020-06-09 16:01:29 +0800

    Cleaning

--- includes/Comms.h
+++
@@ -1,6 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-#undef INTERFACE
-struct response *RX(int port);
-int TX(int port,char writebuffer[]);
-int setup_Port();
-extern bool verbose;
